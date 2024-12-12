8b7b7d2ed1c02e799dea06a0b2e5be8c614d2451
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2024-12-12 16:43:26 +0300

    feat(nav):add header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let title;
+</script>
+
+<header class="header">
+  <h1>{title}</h1>
+</header>
+
+<style>
+  .header {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+</style>
