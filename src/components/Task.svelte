ea2f36fa9ceabaf319603a2192ef9817ae7476a7
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2024-12-12 16:44:23 +0300

    feat(tasks):add task list components

---
+++ src/components/Task.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let task;
+  export let onToggle;
+  export let onDelete;
+
+  const toggleTask = () => onToggle(task.id);
+  const deleteTask = () => onDelete(task.id);
+</script>
+
+<div class="task {task.completed ? 'completed' : ''}">
+  <input type="checkbox" checked={task.completed} on:change={toggleTask} />
+  <span>{task.title}</span>
+  <button on:click={deleteTask}>Delete</button>
+</div>
+
+<style>
+  .task {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 5px 0;
+  }
+  .completed span {
+    text-decoration: line-through;
+  }
+</style>
---
+++ src/components/TaskList.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Task from './Task.svelte';
+
+  export let tasks = [];
+  export let onToggle;
+  export let onDelete;
+</script>
+
+<div>
+  {#each tasks as task (task.id)}
+    <Task {task} {onToggle} {onDelete} />
+  {/each}
+</div>
